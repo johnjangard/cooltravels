e8431e5fb88616c4a93a659f0b1c73332efe3ac8
John <jangardjohn@gmail.com>
2022-05-10 15:38:31 +0200

    Implemented functionality of displaying blog posts correctly

--- front-page.php
+++ front-page.php
@@ -120,73 +120,73 @@ get_header();
     </div>
 </div>
 
-                    </div>
-                    <div class="bg-image d-flex flex-column justify-content-center align-items-center" style="background-image: url('https://mdbcdn.b-cdn.net/img/new/fluid/nature/015.webp'); height: 100vh;">
-                        <h1 class="text-white">Cool resa på rea typ</h1>
-                        <p class="text-white">Säljande text</p>
-                        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">läs mer fyfan</button>
-                    </div>
+</div>
+<div class="bg-image d-flex flex-column justify-content-center align-items-center" style="background-image: url('https://mdbcdn.b-cdn.net/img/new/fluid/nature/015.webp'); height: 100vh;">
+    <h1 class="text-white">Cool resa på rea typ</h1>
+    <p class="text-white">Säljande text</p>
+    <button type="button" class="btn btn-primary btn-lg px-4 gap-3">läs mer fyfan</button>
+</div>
 
-                    <div class="d-flex flex-column align-items-center bg-success pt-3 pb-3 mt-3 mb-3">
-                        <h2>Nyhetsbrev?</h2>
-                        <form class="d-flex flex-column" style="width: 20rem;">
-                            <div class="d-flex flex-row">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
-                                    <label class="form-check-label">Träningsläger</label>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
-                                    <label class="form-check-label">Cuper</label>
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
-                                    <label class="form-check-label">Fotbollsresor</label>
-                                </div>
-                            </div>
-                            <div class="d-flex flex-row">
-                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                <button class="btn btn-outline-warning" type="submit">Search</button>
-                            </div>
-                        </form>
-                    </div>
+<div class="d-flex flex-column align-items-center bg-success pt-3 pb-3 mt-3 mb-3">
+    <h2>Nyhetsbrev?</h2>
+    <form class="d-flex flex-column" style="width: 20rem;">
+        <div class="d-flex flex-row">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                <label class="form-check-label">Träningsläger</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                <label class="form-check-label">Cuper</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                <label class="form-check-label">Fotbollsresor</label>
+            </div>
+        </div>
+        <div class="d-flex flex-row">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-warning" type="submit">Search</button>
+        </div>
+    </form>
+</div>
 
-                    <div class="bg-success pt-3 pb-5 mt-3 mb-3 shadow-lg">
-                        <h2 class="d-flex justify-content-center mb-3">Resekategorier</h2>
-                        <div class="d-flex flex-row justify-content-evenly">
-                            <div class="card shadow-lg" style="width: 27%;">
-                                <h2 class="d-flex justify-content-center mb-3 mt-3">Träningsläger</h2>
-                                <img src="/wp-content/themes/cooltheme/assets/images/Scaredchicken.jpeg" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Träningsläger</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Läs mer</a>
-                                </div>
-                            </div>
-                            <div class="card shadow-lg" style="width: 27%;">
-                                <h2 class="d-flex justify-content-center mb-3 mt-3">Cuper</h2>
-                                <img src="/wp-content/themes/cooltheme/assets/images/Scaredchicken.jpeg" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Cuper</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Läs mer</a>
-                                </div>
-                            </div>
-                            <div class="card shadow-lg" style="width: 27%;">
-                                <h2 class="d-flex justify-content-center mb-3 mt-3">Fotbollssresor</h2>
-                                <img src="/wp-content/themes/cooltheme/assets/images/Scaredchicken.jpeg" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Fotbollsresor</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Läs mer</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+<div class="bg-success pt-3 pb-5 mt-3 mb-3 shadow-lg">
+    <h2 class="d-flex justify-content-center mb-3">Resekategorier</h2>
+    <div class="d-flex flex-row justify-content-evenly">
+        <div class="card shadow-lg" style="width: 27%;">
+            <h2 class="d-flex justify-content-center mb-3 mt-3">Träningsläger</h2>
+            <img src="/wp-content/themes/cooltheme/assets/images/Scaredchicken.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Träningsläger</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Läs mer</a>
+            </div>
+        </div>
+        <div class="card shadow-lg" style="width: 27%;">
+            <h2 class="d-flex justify-content-center mb-3 mt-3">Cuper</h2>
+            <img src="/wp-content/themes/cooltheme/assets/images/Scaredchicken.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Cuper</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Läs mer</a>
+            </div>
+        </div>
+        <div class="card shadow-lg" style="width: 27%;">
+            <h2 class="d-flex justify-content-center mb-3 mt-3">Fotbollssresor</h2>
+            <img src="/wp-content/themes/cooltheme/assets/images/Scaredchicken.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Fotbollsresor</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Läs mer</a>
+            </div>
+        </div>
+    </div>
+</div>
 
 
-                    <?php
-                    get_footer();
+<?php
+get_footer();
 
 // wp_head();
 // the_terms($post->ID, 'topics', 'Topics: ', ', ', ' ');
