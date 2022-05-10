08140fe4ea53b0fed44f9bcc458b47100d65ce8d
Carl Johansson <carljohansson.1992@gmail.com>
2022-05-10 16:38:05 +0200

    fixed typo in CPT-templates

--- cup.php
+++ cup.php
@@ -9,5 +9,6 @@ $loop = new WP_Query(array('post_type' => 'travel_cup', 'posts_per_page' => 10))
 while ($loop->have_posts()) : $loop->the_post();
 
     the_title('<h2 class="entry-title"><a href="' . get_permalink() . '" title="' . the_title_attribute('echo=0') . '" rel="bookmark">', '</a></h2>');
-    get_footer();
+
 endwhile;
+get_footer();
