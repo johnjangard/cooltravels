600b00c71b606c491caa4c34b4156f679972ae72
John <jangardjohn@gmail.com>
2022-05-11 14:02:53 +0200

    Implemented dynamic links and buttons to category indexes, also removed post limits on said indexes

--- soccer.php
+++ soccer.php
@@ -4,11 +4,25 @@ Template Name: Soccer
 */
 get_header();
 
-$loop = new WP_Query(array('post_type' => 'travel_soccer', 'posts_per_page' => 10));
+$loop = new WP_Query(array('post_type' => 'travel_soccer'));
 
 while ($loop->have_posts()) : $loop->the_post();
 
-    the_title('<h2 class="entry-title"><a href="' . get_permalink() . '" title="' . the_title_attribute('echo=0') . '" rel="bookmark">', '</a></h2>');
+?>
+    <div class="card shadow-lg" style="width: 27%;">
+        <img src="/wp-content/themes/cooltheme/assets/images/Scaredchicken.jpeg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <!-- <h5 class="card-title">Card title</h5> -->
+            <?php
+            the_title('<h5 class="card-title"><a href="' . get_permalink() . '" title="' . the_title_attribute('echo=0') . '" rel="bookmark">', '</a></h5>'); ?>
+            <a href="<?php echo get_permalink($post->ID); ?>" class=" btn btn-primary">Klicka här</a>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+        </div>
+    </div>
+<?php
 
 endwhile;
+
 get_footer();
+?>
